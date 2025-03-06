59f27c20655d0225c7a367393abe5be177838b4f
superview <onebetterlogic@gmail.com>
2025-03-05 21:07:37 -0800

    first

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -4,6 +4,11 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise1 Project.");
+        Console.WriteLine("What is your first name? ");
+        string fname = Console.ReadLine();
+        Console.WriteLine("What is your last name? ");
+        string lname = Console.ReadLine();
+
+        Console.WriteLine($"Your name is {fname}, {fname} {lname}.");
     }
 }
