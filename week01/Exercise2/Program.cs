0b55f1147433e90d29b4f9721b29395d6173c1cd
Rick Paul <rpaul@byupathway.edu>
2025-03-08 20:39:51 -0800

    Ex3

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,53 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        string grade = "tt";
+        string valueInText = "42";
+        int int_grade = 0;
+
+        Console.Write("Hello.\nWhat is your grade percentage? ");
+        valueInText = Console.ReadLine();
+        int_grade = int.Parse(valueInText);
+
+        if (int_grade >= 90)
+        {
+            grade = "A";
+        }
+        else if (int_grade >= 80)
+        {
+            grade = "B";
+        }
+
+        else if (int_grade >= 70)
+        {
+            grade = "C";
+        }
+
+        else if (int_grade >= 60)
+        {
+            grade = "D";
+        }
+
+        else if (int_grade < 60)
+        {
+            grade = "F";
+        }
+
+        else
+        {
+            Console.WriteLine("Something went wrong");
+        }
+        
+        Console.WriteLine("Your grade is: " + grade + );
+
+        if (int_grade > 70)
+        {
+            Console.WriteLine("Congratulations you passed.");
+        }
+        else
+        {
+            Console.WriteLine("Remember to keep trying.");
+        }
+
     }
 }
--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,52 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        // Console.ReadLine returns a string object
+        string response = "";
+        int myGuess = -1;
+
+
+        // Get magic number from user
+        Console.WriteLine("What is the magic number? (or any character to auto-generate a random number)");
+        response = Console.ReadLine();
+        if (! int.TryParse(response, out int magic))
+        {
+            Console.WriteLine("Generating a random number between 1 and 100");
+            Random randomGenerator = new Random();
+
+            magic = randomGenerator.Next(1, 101);
+        }
+        else
+        {
+            // Convert the input string to a number
+            magic = int.Parse(response);
+        }
+
+        while(myGuess != magic)
+        {
+            // Get guess from user
+            Console.WriteLine("What is your guess number? ");
+            response = Console.ReadLine();
+
+            // Convert the string to a number
+            myGuess = int.Parse(response);
+
+            if(myGuess == magic)
+            {
+                Console.WriteLine("You guessed it!");
+            }
+            else if(myGuess > magic)
+            {
+                Console.WriteLine("Lower");
+            }
+            else if (myGuess < magic)
+            {
+                Console.WriteLine("Higher");
+            }
+            else
+            {
+                Console.WriteLine("Something went wrong.");
+            }
+        }
     }
 }
