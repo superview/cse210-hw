be6c86554e07c2c3fa258b16e1ee1e02fe331aea
Rick Paul <rpaul@byupathway.edu>
2025-03-12 21:32:44 -0700

    Prompt class working with a private list.

--- week02/Journal/Prompt.cs
+++ week02/Journal/Prompt.cs
@@ -1,12 +1,21 @@
 using System;
+using System.Runtime.CompilerServices;
 using System.Security.Cryptography.X509Certificates;
 
 public class Prompt
 {
+    private List<string> _prompts = new List<string> {
+        "What did you do today?"
+        , "What are you happy about today?"
+        , "What are you sad about today?"
+        , "What new thing did you learn about today?"
+        , "What are you grateful for today?"
+        , "Who did you lift up today?"
+    };
     // Prompts constructor
     public Prompt()
     {
-        Console.WriteLine("Prompts constructor.");
+        Console.WriteLine($"Prompts constructor.");
 
     }
 
@@ -17,14 +26,6 @@ public class Prompt
     public string GetRandom()
     {
         Console.WriteLine("Getting random string.");
-        List<string> _prompts = new List<string> {
-            "What did you do today?"
-            , "What are you happy about today?"
-            , "What are you sad about today?"
-            , "What new thing did you learn about today?"
-            , "What are you grateful for today?"
-            , "Who did you lift up today?"
-        };
         Random rand_number = new Random();
         int index = rand_number.Next(_prompts.Count);
 
