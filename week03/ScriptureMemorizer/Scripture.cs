711d5467fd0ed71b4de42fddc099fc6544e2c2f7
Rick Paul <rpaul@byupathway.edu>
2025-03-31 22:36:00 -0500

    Final

--- week03/ScriptureMemorizer/Scripture.cs
+++ week03/ScriptureMemorizer/Scripture.cs
@@ -11,7 +11,6 @@ public class Scripture
     public int visible_words;
     public Scripture(Reference ref1, string text)
     {
-        // string file = "scriptures.json";
         _reference = ref1;
 
         // add words using the word object
@@ -20,7 +19,6 @@ public class Scripture
             Words.Add(new Word(w, false));
         }
         visible_words = Words.Count;
-        // Load(file);
     }
 
     public void HideRandomWords()
@@ -28,7 +26,6 @@ public class Scripture
         Random rand = new Random();
         int selected = rand.Next(0, Words.Count);
         bool loop_condition = true;
-        // while(! Words[selected].IsHidden())
         while(loop_condition)
         {
             selected = rand.Next(0, Words.Count);
@@ -39,7 +36,6 @@ public class Scripture
                 loop_condition = false;
                 Console.WriteLine($"This is the number of visible: {visible_words}");
             }
-            // loop_condition = false;
         }
 
     }
@@ -54,66 +50,12 @@ public class Scripture
         return false;
     }
 
-    // add a verse to a scripture
-    // public Verse AddVerse()
-    // {
-    //     Verse aVerse = new();
-
-    //     Console.WriteLine("Enter the book: ");
-    //     string book = Console.ReadLine();
-    //     aVerse.Book = book;
-
-    //     Console.WriteLine("Enter the scripture content: ");
-    //     string content = Console.ReadLine();
-    //     aVerse.Content = content;
-
-    //     Console.WriteLine("Enter the chapter: ");
-    //     string chapter = Console.ReadLine();
-    //     aVerse.Chapter = chapter;
-
-    //     Console.WriteLine("Enter the chapter: ");
-    //     string verse = Console.ReadLine();
-    //     aVerse.Verse_number = verse;
-
-    //     verses.Add(aVerse);
-    //     Console.WriteLine($"Book: {aVerse.Book} {aVerse.Chapter}:{aVerse.Verse_number}");
-    //     Console.WriteLine($"Scripture Text:\n{aVerse.Content}");
-    //     return aVerse;
-    // }
-    // public void Save(string file)
-    // {
-    //     string jsonString = JsonSerializer.Serialize(verses, new JsonSerializerOptions {WriteIndented = true});
-    //     File.WriteAllText(file, jsonString);
-    //     Console.WriteLine(jsonString);
-    //     Console.WriteLine("Saved to file: " + file);
-    // }
-    // public void Load(string file)
-    // {
-    //     try
-    //     {
-    //         string json3 = File.ReadAllText(file);
-    //         verses = JsonSerializer.Deserialize<List<Verse>>(json3);
-    //         Console.WriteLine("\n");
-    //         foreach (var v in verses)
-    //         {
-    //             Console.WriteLine($"This is the reference: {v.Book} {v.Chapter} {v.Verse_number}");
-    //         }
-    //         // Success loading file
-    //         Console.WriteLine($"{file} loaded.");
-    //     }
-    //     catch (IOException ex)
-    //     {
-    //         Console.WriteLine($"Initial load of scriptures could not be completed. {ex.Message}");
-    //     }
-    // }
-   
     public void DisplayAll()
     {
         Console.WriteLine("------------------------------------------\n");
         Console.WriteLine($"Reference: {_reference.DisplayAll()}");
         foreach (Word w in Words)
         {
-            // Console.WriteLine($"{w.GetDisplayText} {w.Chapter}:{w.Verse_number}");
             Console.Write($"{w.GetDisplayText()} ");
         }
         // Write line after quote
